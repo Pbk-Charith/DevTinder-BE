6292910d8ded3e029cd4f6a17a31c4f21d735aee
Pbk-Charith <pbcharith01@gmail.com>
2026-04-07 18:44:12 +0530

    Exploring GET, PUT, POST, DELETE HTTP methods..

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 
-    app.use("/", (req, res) => {
-        res.send("Hello Worldsssssssssssssssssssssssssssssssssssssss charith!");
-    });
 
-app.use("/hello", (req, res) => {
-    res.send("This is the API endpoint.");
+app.get('/user', (req, res) => {
+    res.send("Hello World");
 });
 
-app.use("/hellos", (req, res) => {
-    res.send("This is the API startpoint.");
+app.post('/data', (req, res) => {
+    res.send("Data received");
 });
 
+app.put('/update', (req, res) => {
+    res.send("Data updated");
+});
+
+app.delete('/delete', (req, res) => {
+    res.send("Data deleted");
+});
+
+app.get("/hello", (req, res) => {
+    res.send({Firstname: "Sai Charith", Lastname: "puttabakula"});
+});
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
