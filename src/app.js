695aac9c8db1952fa472d6f251ed76333a81924b
Pbk-Charith <pbcharith01@gmail.com>
2026-04-08 12:40:01 +0530

    Knowing How next() is important

--- src/app.js
+++ src/app.js
@@ -2,26 +2,29 @@ const express = require('express');
 const app = express();
 
 
-app.get("/user/:id/:name/:age/:email", (req, res) => {
-    console.log(req.params);
-    res.send("Hello World");
-});
-
-app.post('/data', (req, res) => {
-    res.send("Data received");
-});
-
-app.put('/update', (req, res) => {
-    res.send("Data updated");
-});
-
-app.delete('/delete', (req, res) => {
-    res.send("Data deleted");
-});
-
-app.get("/hello", (req, res) => {
-    res.send({Firstname: "Sai Charith", Lastname: "puttabakula"});
-});
+app.get("/user", (req, res,next) => {
+    console.log("First response")
+    res.send("First response");
+        next();
+},
+(req, res, next) => { 
+    console.log("Second response")
+    res.send("Second response");
+    next();
+}
+,(req, res,next) => {
+    console.log("Third response")
+    res.send("Third response");
+    next();
+},(req, res,next) => {
+    console.log("Fourth response")
+    res.send("Fourth response");
+    next();
+},(req, res) => {
+    console.log("Fifth response")
+    res.send("Fifth response");
+}
+);
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
