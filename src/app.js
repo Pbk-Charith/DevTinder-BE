c99eab75f928d10f1604594100ba63c3ea70e2f1
Pbk-Charith <pbcharith01@gmail.com>
2026-04-09 11:07:11 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,29 +2,61 @@ const express = require('express');
 const app = express();
 
 
-app.get("/user", (req, res,next) => {
-    console.log("First response")
-    res.send("First response");
-        next();
-},
-(req, res, next) => { 
-    console.log("Second response")
-    res.send("Second response");
+// app.use("/", (req,res, next) => {
+//     // console.log("api1");
+//     res.send("api1");
+//     next();
+// });
+
+// app.get("/api2", 
+//     (req,res,next) => {
+//     console.log("api2");
+//     // res.send("api2");
+//     next();
+// }, 
+// (req,res,next) => {    
+//     console.log("api2-2");
+//     // res.send("api2-2");
+//     next();
+// }, 
+// (req,res,next) => {    
+//     console.log("api2-3");
+//     // res.send("api2-3");
+//     next();
+// }
+// );
+
+// app.use("/hello1",(req, res, next) => {
+//     console.log("Step 1");
+//     // res.send("Hello World1");
+//     next();
+// });
+
+// app.get("/hello2", (req, res, next) => {
+//     console.log("Step 2");
+//     next();
+//     // res.send("Hello World2");
+// });
+app.use((req, res, next) => {
+    // res.send("Hello Worldssss");
+    console.log("Middleware 0");
     next();
-}
-,(req, res,next) => {
-    console.log("Third response")
-    res.send("Third response");
+});
+
+// Middleware 1
+app.use((req, res, next) => {
+    console.log("Middleware 1");
     next();
-},(req, res,next) => {
-    console.log("Fourth response")
-    res.send("Fourth response");
+});
+
+// Middleware 2
+app.use((req, res, next) => {
+    console.log("Middleware 2");
+    res.send("Hello World from Middleware 2");
     next();
-},(req, res) => {
-    console.log("Fifth response")
-    res.send("Fifth response");
-}
-);
+});
+
+// Route
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
