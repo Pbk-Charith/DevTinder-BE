1bd5fed89002b72039132f5064f8594d9df766c8
Pbk-Charith <pbcharith01@gmail.com>
2026-04-13 00:40:17 +0530

    Refactor user retrieval endpoint to handle age-based queries and improve error handling for user deletion

--- src/app.js
+++ src/app.js
@@ -18,33 +18,33 @@ app.post("/Signup", async (req, res) => {
     
 });
 
-// app.get("/user", async (req, res) => {
-//     // const userage = req.body.age;
+app.get("/user", async (req, res) => {
+    const userage = req.body.age;
 
-//     // try {
-//     //     const user = await User.findOne({ age: 52 });
+    try {
+        const user = await User.findOne({ age: 52 });
 
-//     //     if (!user) {
-//     //         return res.status(404).send("No users found with the specified age");
-//     //     }
+        if (!user) {
+            return res.status(404).send("No users found with the specified age");
+        }
 
-//     //     res.status(200).send(user);
-//     // } catch (err) {
-//     //     res.status(500).send("Error fetching users");
-//     // }
+        res.status(200).send(user);
+    } catch (err) {
+        res.status(500).send("Error fetching users");
+    }
 
-//     // try {
-//     //     const users = await User.find({ age: userage });
-//     //     if(users.length === 0){
-//     //         return res.status(404).send("No users found with the specified age");
-//     //     }else{
-//     //         res.status(200).send(users);
-//     //     }
-//     // }
-//     // catch(err){
-//     //     res.status(500).send("Error fetching users");
-//     // }
-// });
+    try {
+        const users = await User.find({ age: userage });
+        if(users.length === 0){
+            return res.status(404).send("No users found with the specified age");
+        }else{
+            res.status(200).send(users);
+        }
+    }
+    catch(err){
+        res.status(500).send("Error fetching users");
+    }
+});
 
 app.get("/feed", async (req, res) => {
 
@@ -56,6 +56,19 @@ app.get("/feed", async (req, res) => {
         res.status(500).send("Error fetching users");
     }
 
+});
+
+app.delete("/user", async (req, res) => {
+    const userid = req.body.id;
+    try{
+        const user = await User.findByIdAndDelete(userid);
+        if(!user){
+            return res.status(404).send("User not found");
+        }
+        res.status(200).send("User deleted successfully");
+    } catch (error) {
+        res.status(500).send("Error deleting user");
+    }
 });
 
 connectDB().then(() => {
